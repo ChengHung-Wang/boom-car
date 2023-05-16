e8b907d77ed4181c15a342a25611e6a2ec5ea0fa
王正宏 <wangzhenghong@ip-12.wt1.ntust.edu.tw>
2023-05-16 22:11:00 +0800

    add X/Y/Z-axis cam slider

--- js/helper.js
+++ js/helper.js
@@ -6,19 +6,34 @@ const appOption = {
     data() {
         return {
             joyStick: null,
-            gameStarted: false
+            gameStarted: false,
+            camera: {
+                x: 0,
+                y: 0,
+                z: 0
+            }
         }
     },
     created() {
-
+        this.camera.y = camera.yOffset;
+        this.camera.z = camera.zOffset;
     },
     methods: {
         startGame(trackNumber) {
             this.gameStarted = true;
             startGame(trackNumber);
         }
+    },
+    watch: {
+        camera: {
+            handler() {
+                camera.adjust(this.camera.y, this.camera.z);
+            },
+            deep: true
+        }
     }
 }
+
 const app = createApp(appOption)
 app.use(ElementPlus);
 app.mount("#app");
