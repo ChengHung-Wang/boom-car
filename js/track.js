3ddfaef88e5be0afbf259fb95e36f2dfd9a3745c
SyuanYu Liao <nalivvo@gmail.com>
2023-05-17 19:02:38 +0800

    Replace prototype with class

--- js/track.js
+++ js/track.js
@@ -5,30 +5,29 @@ var COLORS_KERBLIGHT = "#a02222",
   COLORS_LANDDARK = "#000000",
   COLORS_ROAD = "#000000";
 
-var Track = function () {
-  this.trackLength = 0;
-  this.currentAngle = 0;
-
-  this.segments = [];
-  this.map = null;
-};
-
-var laneWidth = 1200; // 1200;//2000;
-
-Track.segmentLength = 300;
-
+var laneWidth = 1200;
 var lanes = 1;
 
-Track.prototype = {
-  buildTrack0: function () {
+class Track {
+  constructor() {
+    this.trackLength = 0;
+    this.currentAngle = 0;
+
+    this.segments = [];
+    this.map = null;
+
+    Track.segmentLength = 300;
+  }
+
+  buildTrack0() {
     COLORS_FOG = 0;
 
     this.segments = [];
     this.addStraight(200);
     this.calculateLength();
-  },
+  }
 
-  createStreetLights: function () {
+  createStreetLights() {
     var segmentCount = this.getSegmentCount();
 
     for (var i = 0; i < segmentCount; i++) {
@@ -50,9 +49,9 @@ Track.prototype = {
         });
       }
     }
-  },
+  }
 
-  createRoadsideObjects: function (objs, prob, scale, offset, turnSigns) {
+  createRoadsideObjects(objs, prob, scale, offset, turnSigns) {
     var segmentCount = this.getSegmentCount();
     var turnSegment = 0;
     for (var i = 0; i < segmentCount; i++) {
@@ -99,9 +98,9 @@ Track.prototype = {
         }
       }
     }
-  },
+  }
 
-  buildTrack1: function () {
+  buildTrack1() {
     COLORS_ROAD = "#3a3a3a";
 
     (COLORS_LANDLIGHT = "#047804"), (COLORS_LANDDARK = "#006A00");
@@ -116,38 +115,36 @@ Track.prototype = {
     createBackgroundMountains();
     createCars();
 
-    var t = this;
+    this.addStraight(50); // len, height
+    this.addEasyCurve90(1, 0);
+    this.addRoad(50, 50, 39, 0, 40, 0);
 
-    t.addStraight(50); // len, height
-    t.addEasyCurve90(1, 0);
-    t.addRoad(50, 50, 39, 0, 40, 0);
+    this.addEasyCurve90(1, 0);
 
-    t.addEasyCurve90(1, 0);
+    this.addStraight(25);
 
-    t.addStraight(25);
+    this.addEasyCurve30(-1, 0);
+    this.addEasyCurve30(1, 0);
 
-    t.addEasyCurve30(-1, 0);
-    t.addEasyCurve30(1, 0);
+    this.addHill(50, 40);
 
-    t.addHill(50, 40);
+    this.addEasyCurve90(1, 0);
 
-    t.addEasyCurve90(1, 0);
+    this.addEasyCurve30(-1, 0);
+    this.addEasyCurve30(1, 0);
 
-    t.addEasyCurve30(-1, 0);
-    t.addEasyCurve30(1, 0);
+    this.addEasyCurve90(1, -40);
 
-    t.addEasyCurve90(1, -40);
+    this.addStraight(50, -40);
+    this.addStraight(55, 0);
 
-    t.addStraight(50, -40);
-    t.addStraight(55, 0);
+    this.calculateLength();
+    this.drawMap();
 
-    t.calculateLength();
-    t.drawMap();
+    this.createRoadsideObjects(SPRITES_TREES, 0.9, 10, 900, true);
+  }
 
-    t.createRoadsideObjects(SPRITES_TREES, 0.9, 10, 900, true);
-  },
-
-  buildTrack2: function () {
+  buildTrack2() {
     COLORS_ROAD = "#3a3a3a";
 
     COLORS_LANDLIGHT = "#047804";
@@ -164,39 +161,38 @@ Track.prototype = {
     createBackgroundMountains();
     createFlowers();
 
-    var t = this;
-    t.addStraight(20);
-    t.addStraight(46, 0);
-    t.addEasyCurve90(1, 30);
+    this.addStraight(20);
+    this.addStraight(46, 0);
+    this.addEasyCurve90(1, 30);
 
-    t.addStraight(90, 0);
-    t.addMediumCurve90(1, 0);
+    this.addStraight(90, 0);
+    this.addMediumCurve90(1, 0);
 
-    t.addStraight(25, 0);
-    t.addMediumCurve90(1, 50);
-    t.addStraight(25, 0);
+    this.addStraight(25, 0);
+    this.addMediumCurve90(1, 50);
+    this.addStraight(25, 0);
 
-    t.addMediumCurve90(-1, 0);
-    t.addStraight(68, -50);
+    this.addMediumCurve90(-1, 0);
+    this.addStraight(68, -50);
 
-    t.addMediumCurve90(-1, 0);
-    t.addMediumCurve90(1, 0);
-    t.addMediumCurve90(1, 0);
-    t.addStraight(48, 0);
+    this.addMediumCurve90(-1, 0);
+    this.addMediumCurve90(1, 0);
+    this.addMediumCurve90(1, 0);
+    this.addStraight(48, 0);
 
-    t.addEasyCurve90(1, -30);
-    t.addStraight(38, 0);
+    this.addEasyCurve90(1, -30);
+    this.addStraight(38, 0);
 
-    t.addEasyCurve30(-1, 0);
-    t.addEasyCurve30(1, 0);
+    this.addEasyCurve30(-1, 0);
+    this.addEasyCurve30(1, 0);
 
-    t.calculateLength();
-    t.drawMap();
+    this.calculateLength();
+    this.drawMap();
 
-    t.createRoadsideObjects([SPRITES_FLOWERS], 0.3, 6, 1300, true);
-  },
+    this.createRoadsideObjects([SPRITES_FLOWERS], 0.3, 6, 1300, true);
+  }
 
-  buildTrack3: function () {
+  buildTrack3() {
     COLORS_ROAD = "#3a3a3a";
 
     COLORS_LANDLIGHT = "#5a5a5a";
@@ -213,36 +209,33 @@ Track.prototype = {
     createBackgroundBuildings(false);
     createNightSky();
 
-    var t = this;
+    this.addStraight(100);
+    this.addMediumCurve90(1, 0);
+    this.addStraight(151, 0);
+    this.addHardCurve90(1, 0);
+    this.addStraight(30, 0);
+    this.addHardCurve90(1, 0);
+    this.addStraight(80, 0);
+    this.addMediumCurve90(-1, 0);
+    this.addMediumCurve90(-1, 0);
 
-    t.addStraight(100);
-    t.addMediumCurve90(1, 0);
-    t.addStraight(151, 0);
-    t.addHardCurve90(1, 0);
-    t.addStraight(30, 0);
-    t.addHardCurve90(1, 0);
-    t.addStraight(80, 0);
-    t.addMediumCurve90(-1, 0);
-    t.addMediumCurve90(-1, 0);
+    this.addStraight(20, 0);
+    this.addMediumCurve90(1, 0);
+    this.addStraight(10, 0);
+    this.addHardCurve90(1, 0);
+    this.addStraight(50, 0);
+    this.addMediumCurve90(-1, 0);
+    this.addMediumCurve90(1, 0);
+    this.addMediumCurve90(1, 0);
+    this.addStraight(62, 0);
 
-    t.addStraight(20, 0);
-    t.addMediumCurve90(1, 0);
-    t.addStraight(10, 0);
-    t.addHardCurve90(1, 0);
-    t.addStraight(50, 0);
-    t.addMediumCurve90(-1, 0);
-    t.addMediumCurve90(1, 0);
-    t.addMediumCurve90(1, 0);
-    t.addStraight(62, 0);
+    this.calculateLength();
+    this.drawMap();
+    this.createRoadsideObjects(SPRITES_BUILDINGS, 0.95, 20, 3300, false);
+    this.createStreetLights();
+  }
 
-    //    t.getSegment(0).color = COLORS.START;
-    t.calculateLength();
-    t.drawMap();
-    t.createRoadsideObjects(SPRITES_BUILDINGS, 0.95, 20, 3300, false);
-    t.createStreetLights();
-  },
-
-  buildTrack4: function () {
+  buildTrack4() {
     COLORS_ROAD = "#111111";
     COLORS_LANEMARKER = "#555555";
     COLORS_FOG = "#000000";
@@ -258,55 +251,54 @@ Track.prototype = {
     // createBackgroundMountains();
     createNightSky();
 
-    var t = this;
-    t.addStraight(100);
-    t.addHardCurve180(1, 0);
-    t.addHardCurve90(-1, 0);
-    t.addStraight(40, 0);
-    t.addHardCurve90(1, 0);
-    t.addHardCurve90(-1, 0);
-    t.addHardCurve90(1, 0);
-    t.addStraight(50, 0);
-    t.addMediumCurve90(-1, 0);
-    t.addStraight(20, 0);
-    t.addMediumCurve90(1, 0);
-    t.addHardCurve90(1, 0);
-    t.addStraight(60, 0);
-    t.addMediumCurve90(-1, 0);
-    t.addMediumCurve90(1, 0);
-    t.addStraight(51, 0);
-    t.addHardCurve90(1, 0);
-    t.addStraight(110, 0);
-    t.calculateLength();
-    t.drawMap();
+    this.addStraight(100);
+    this.addHardCurve180(1, 0);
+    this.addHardCurve90(-1, 0);
+    this.addStraight(40, 0);
+    this.addHardCurve90(1, 0);
+    this.addHardCurve90(-1, 0);
+    this.addHardCurve90(1, 0);
+    this.addStraight(50, 0);
+    this.addMediumCurve90(-1, 0);
+    this.addStraight(20, 0);
+    this.addMediumCurve90(1, 0);
+    this.addHardCurve90(1, 0);
+    this.addStraight(60, 0);
+    this.addMediumCurve90(-1, 0);
+    this.addMediumCurve90(1, 0);
+    this.addStraight(51, 0);
+    this.addHardCurve90(1, 0);
+    this.addStraight(110, 0);
+    this.calculateLength();
+    this.drawMap();
 
-    t.createRoadsideObjects(SPRITES_BUILDINGS, 0.95, 20, 3300, false);
-    t.createStreetLights();
-  },
+    this.createRoadsideObjects(SPRITES_BUILDINGS, 0.95, 20, 3300, false);
+    this.createStreetLights();
+  }
 
-  lastY: function () {
+  lastY() {
     return this.segments.length == 0
       ? 0
       : this.segments[this.segments.length - 1].p3.world.y;
-  },
+  }
 
-  getSegment: function (index) {
+  getSegment(index) {
     return this.segments[index];
-  },
+  }
 
-  getSegmentCount: function () {
+  getSegmentCount() {
     return this.segments.length;
-  },
+  }
 
-  getLength: function () {
+  getLength() {
     return this.trackLength;
-  },
+  }
 
-  calculateLength: function () {
-    this.trackLength = track.segments.length * Track.segmentLength;
-  },
+  calculateLength() {
+    this.trackLength = this.segments.length * Track.segmentLength;
+  }
 
-  addSegment: function (curve, y) {
+  addSegment(curve, y) {
     var n = this.segments.length;
 
     var yFront = this.lastY();
@@ -339,19 +331,21 @@ Track.prototype = {
       sprites: [],
       cars: [],
     });
-  },
+  }
 
-  easeIn: function (a, b, percent) {
+  easeIn(a, b, percent) {
     return a + (b - a) * Math.pow(percent, 2);
-  },
-  easeOut: function (a, b, percent) {
-    return a + (b - a) * (1 - Math.pow(1 - percent, 2));
-  },
-  easeInOut: function (a, b, percent) {
-    return a + (b - a) * (-Math.cos(percent * Math.PI) / 2 + 0.5);
-  },
+  }
 
-  addRoad: function (enter, hold, leave, curve, y, curveAngle) {
+  easeOut(a, b, percent) {
+    return a + (b - a) * (1 - Math.pow(1 - percent, 2));
+  }
+
+  easeInOut(a, b, percent) {
+    return a + (b - a) * (-Math.cos(percent * Math.PI) / 2 + 0.5);
+  }
+
+  addRoad(enter, hold, leave, curve, y, curveAngle) {
     var curveAngle = curveAngle || 0;
     var exitAngle = this.currentAngle + curveAngle;
 
@@ -394,14 +388,14 @@ Track.prototype = {
 
     this.currentAngle = exitAngle;
     this.segments[this.segments.length - 1].angle = exitAngle;
-  },
+  }
 
-  addStraight: function (len, height) {
+  addStraight(len, height) {
     height = height || 0;
     this.addRoad(len, len, len, 0, height, 0);
-  },
+  }
 
-  addBumps: function () {
+  addBumps() {
     this.addRoad(10, 10, 10, 0, 5);
     this.addRoad(10, 10, 10, 0, -2);
     this.addRoad(10, 10, 10, 0, -5);
@@ -410,17 +404,17 @@ Track.prototype = {
     this.addRoad(10, 10, 10, 0, -7);
     this.addRoad(10, 10, 10, 0, 5);
     this.addRoad(10, 10, 10, 0, -2);
-  },
+  }
 
-  addEasyCurve90: function (direction, height) {
+  addEasyCurve90(direction, height) {
     this.addRoad(25, 100 * 1.4, 25, direction * 4.25, height, direction * 90);
-  },
+  }
 
-  addEasyCurve30: function (direction, height) {
+  addEasyCurve30(direction, height) {
     this.addRoad(25, 50, 25, direction * 4.25, height, direction * 30);
-  },
+  }
 
-  addMediumCurve90: function (direction, height) {
+  addMediumCurve90(direction, height) {
     this.addRoad(
       25,
       50 * 1.5,
@@ -429,21 +423,22 @@ Track.prototype = {
       height,
       direction * 90
     );
-  },
+  }
 
-  addHardCurve90: function (direction) {
+  addHardCurve90(direction) {
     //7.5
     this.addRoad(18, 50 * 0.8, 18, direction * 8, 0, direction * 90);
-  },
-  addHardCurve180: function () {
+  }
+
+  addHardCurve180() {
     this.addRoad(50, 50, 50, 7.5, 0, 180);
-  },
+  }
 
-  addHill: function (num, height) {
+  addHill(num, height) {
     this.addRoad(num, num, num, 0, height, 0);
-  },
+  }
 
-  addRoadsideObject: function (n, sprite, offset) {
+  addRoadsideObject(n, sprite, offset) {
     var segment = this.segments[n];
     var x = 0;
     if (offset < 0) {
@@ -452,20 +447,15 @@ Track.prototype = {
       x = segment.p2.world.x + 600;
     }
     segment.sprites.push({ source: sprite, x: x });
-  },
+  }
 
-  /*
-  When the car reaches the end of the road we will simply loop back to the beginning. 
-  To make this a little easier we provide a method to find the segment for any Z value
-  even if it extends beyond the length of the road:
-  */
-  findSegment: function (z) {
+  findSegment(z) {
     return this.segments[
       Math.floor(z / Track.segmentLength) % this.segments.length
     ];
-  },
+  }
 
-  drawMap: function () {
+  drawMap() {
     if (this.map == null) {
       this.map = document.createElement("canvas");
     }
@@ -519,9 +509,9 @@ Track.prototype = {
     cntxLineTo(x, y);
 
     cntxStroke();
-  },
+  }
 
-  drawOverheadTrack: function () {
+  drawOverheadTrack() {
     //var canvas = document.getElementById('trackCanvas');
     cntx = overheadTrack.x; //canvas.getContext('2d');
     this.overheadMap = overheadTrack.c;
@@ -532,7 +522,7 @@ Track.prototype = {
     // opponents
     for (var i = 0; i < cars.length; i++) {
       var carPosition = cars[i].z;
-      var segment = track.findSegment(carPosition);
+      var segment = this.findSegment(carPosition);
 
       cntxBeginPath();
 
@@ -546,7 +536,7 @@ Track.prototype = {
 
     // camera z position plus player z position from camera
     var playerPosition = cars[0].z;
-    var playerSegment = track.findSegment(playerPosition);
+    var playerSegment = this.findSegment(playerPosition);
 
     cntxBeginPath();
     cntxArc(playerSegment.x, playerSegment.y, 5, 0, 2 * PI, false);
@@ -556,5 +546,5 @@ Track.prototype = {
     context.lineWidth = 2;
     cntxStrokeStyle(MEDIUMGREY);
     cntxStroke();
-  },
-};
+  }
+}
