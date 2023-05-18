b2e626398c78195b0635f38318000d9de5539639
Hansen Chien <32165157+hansen033@users.noreply.github.com>
2023-05-18 19:55:51 +0800

    註解log，會淹沒

--- js/callback.js
+++ js/callback.js
@@ -1,11 +1,11 @@
 function callback_RaceOver(){
-    console.log("callback_RaceOver");
+    //console.log("callback_RaceOver");
 }
 
 function callback_LapOver(){
-    console.log("callback_LapOver");
+    //console.log("callback_LapOver");
 }
 
 function callback_SpeedChange(speed){
-    console.log("callback_SpeedChange: ", speed);
+    //console.log("callback_SpeedChange: ", speed);
 }
