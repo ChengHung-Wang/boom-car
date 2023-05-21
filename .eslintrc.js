5aa616f4c01f3e09eb8965f7455618161bbbea96
王正宏 <wangzhenghong@ip-12.wt1.ntust.edu.tw>
2023-05-22 03:08:20 +0800

    add module support for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
     },
     "extends": [
         "eslint:recommended",
     ],
-    "overrides": [
-    ],
+    "overrides": [],
     "parserOptions": {
-        "ecmaVersion": "latest"
+        "ecmaVersion": "latest",
+        "sourceType": "module",
     },
     "rules": {
         "no-var": "error",
         // Disallow assignment statements in if, for, and while unless the assignment statement is enclosed in parentheses
         "no-cond-assign": [
             "error",
-            "except-parens"
+            "except-parens",
         ],
         // Disallow constants as test conditions in if, for, while, e.g. if (true), for (;;) , unless there is a break statement inside the loop
         "no-constant-condition": [
             "error",
             {
-                checkLoops: false
-            }
+                checkLoops: false,
+            },
         ],
-        "no-compare-neg-zero": "error"
+        "no-compare-neg-zero": "error",
     }
 }
