9bf7cbbd77a8c814817839fcbe913c7d87ff2363
王正宏 <wangzhenghong@ip-12.wt1.ntust.edu.tw>
2023-05-22 00:51:33 +0800

    eslint plugin add

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        "no-var": "error",
+        // Disallow assignment statements in if, for, and while unless the assignment statement is enclosed in parentheses
+        "no-cond-assign": [
+            "error",
+            "except-parens"
+        ],
+        // Disallow constants as test conditions in if, for, while, e.g. if (true), for (;;) , unless there is a break statement inside the loop
+        "no-constant-condition": [
+            "error",
+            {
+                checkLoops: false
+            }
+        ],
+        "no-compare-neg-zero": "error"
+    }
+}
