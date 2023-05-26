623bdba6cb24102f12c61af9f0488d1a70b0fbec
Nolan Liao <nalivvo@gmail.com>
2023-05-26 19:37:55 +0800

    Check audio.js

--- js/audio.js
+++ js/audio.js
@@ -24,27 +24,20 @@ export function raceAudioInit() {
     raceAudioCreateTurboBuffer();
     raceAudioCreateNoiseBuffer();
 
-    //    drawBuffer(audioTurboData);
-
-
     audioScriptNode = audioCtx.createScriptProcessor(1024, 1, 1);
     audioScriptNode.onaudioprocess = function (e) {
-      //audioEngineData = audioTurboData;
-
       const channel = e.outputBuffer.getChannelData(0);
       let index;
 
       for (let i = 0; i < channel.length; ++i) {
         // skip more data frames on higher speed
-
         if (racer.player.turbo) {
           audioEngineFrame += audioScriptSpeed + Math.random();
           audioTurboFrame += audioTurboSpeed;
           index = Math.floor(audioTurboFrame) % audioTurboData.length;
-          channel[i] = audioTurboData[index];// + Math.random() * 0.2;
+          channel[i] = audioTurboData[index];
           index = Math.floor(audioEngineFrame) % audioEngineData.length;
           channel[i] += audioEngineData[index] + Math.random() * 0.01;
-
         } else {
           audioEngineFrame += audioScriptSpeed + Math.random() * 1;
           index = Math.floor(audioEngineFrame) % audioEngineData.length;
@@ -54,12 +47,9 @@ export function raceAudioInit() {
         if (racer.player.slipstreamTime > 0) {
           channel[i] += Math.random() * 0.4;
         }
-        //channel[i] += audioEngineData[index] + Math.random() * 0.01;
       }
       audioEngineFrame %= audioEngineData.length;
       audioTurboFrame %= audioTurboData.length;
-      //      audioScriptCurrentFrame %= audioEngineData.length;
-
     }
     audioScriptGain = audioCtx.createGain();
     audioScriptGain.gain.value = 0.14;
@@ -69,12 +59,11 @@ export function raceAudioInit() {
 }
 
 function raceAudioCreateTurboBuffer() {
-  const bufferSize = 1024; // 2 * audioCtx.sampleRate;
+  const bufferSize = 1024;
   audioTurboData = [];
   let index = 0;
 
-  for (let i = 0; i < bufferSize; i++) {
-    //    audioTurboData[i] = Math.random();
+  for (let i = 0; i < bufferSize; ++i) {
     for (let j = 0; j < 12; j++) {
       audioTurboData[index++] = Math.random() * 0.01;
       if (index >= bufferSize) {
@@ -85,9 +74,8 @@ function raceAudioCreateTurboBuffer() {
     const v = 0.2;
 
     if (index < bufferSize) {
-      for (let k = 0; k < 2; k++) {
+      for (let k = 0; k < 2; ++k) {
         audioTurboData[index++] = v;
-
         if (index >= bufferSize) {
           break;
         }
@@ -95,8 +83,8 @@ function raceAudioCreateTurboBuffer() {
     }
   }
 
-  for (let i = 0; i < bufferSize; i++) {
-    audioTurboData[i] += Math.random() * 0.5 - 0.05;// pinkNoise[Math.floor(i/4)];
+  for (let i = 0; i < bufferSize; ++i) {
+    audioTurboData[i] += Math.random() * 0.5 - 0.05;
   }
 }
 
@@ -105,7 +93,6 @@ function raceAudioCreateEngineBuffer() {
   audioEngineData = [];
 
   let lastValue = 1;
-  //var lastPosition = 0;
   let nextValue, nextPosition;
 
   let index = 0;
@@ -116,7 +103,7 @@ function raceAudioCreateEngineBuffer() {
     nextValue = Math.random() * 2 - 1;
     const positionDiff = nextPosition - (index - 1);
     const step = (nextValue - lastValue) / positionDiff;
-    for (let j = 0; j < positionDiff; j++) {
+    for (let j = 0; j < positionDiff; ++j) {
       audioEngineData[index++] = lastValue + step * j;
     }
     lastValue = nextValue;
@@ -124,7 +111,7 @@ function raceAudioCreateEngineBuffer() {
 
   const positionDiff = bufferSize - (index - 1);
   const step = (1 - lastValue) / positionDiff;
-  for (let j = 0; j < positionDiff; j++) {
+  for (let j = 0; j < positionDiff; ++j) {
     audioEngineData[index++] = lastValue + step * j;
   }
 }
@@ -134,15 +121,15 @@ function raceAudioCreateNoiseBuffer() {
   noiseBuffer = audioCtx.createBuffer(1, bufferSize, audioCtx.sampleRate);
   const output = noiseBuffer.getChannelData(0);
 
-  for (let i = 0; i < bufferSize; i++) {
+  for (let i = 0; i < bufferSize; ++i) {
     output[i] = Math.random() * 2 - 1;
   }
 }
 
 export function raceAudioTone(freq, duration) {
   const gain = audioCtx.createGain();
-
   const osc = audioCtx.createOscillator();
+
   osc.connect(gain);
   gain.connect(audioCtx.destination);
   osc.type = "triangle";//"sawtooth";
@@ -156,16 +143,11 @@ export function raceAudioEngineSpeed(speed) {
   audioScriptSpeed = 0.2 + speed * 2;
 }
 
-// function raceAudioEngineStop() {
-// }
-
 let lastCrashTime = 0;
-
 export function raceAudioCrash() {
   const crashTime = racer.getTimestamp();
   if (crashTime - lastCrashTime < 1000) {
     return;
-
   }
   lastCrashTime = crashTime;
 
@@ -179,7 +161,7 @@ export function raceAudioCrash() {
 
   audioSource.buffer = noiseBuffer;
 
-  gain.gain.linearRampToValueAtTime(0.5, audioCtx.currentTime);//+ 1/64);
+  gain.gain.linearRampToValueAtTime(0.5, audioCtx.currentTime);
   gain.gain.linearRampToValueAtTime(0, audioCtx.currentTime + noteLength * 0.7);
 
   audioSource.playbackRate.setValueAtTime(0.035, audioCtx.currentTime);
@@ -187,18 +169,3 @@ export function raceAudioCrash() {
   audioSource.start(audioCtx.currentTime);
   audioSource.stop(audioCtx.currentTime + noteLength);
 }
-
-// function drawBuffer(buffer) {
-//   const canvas = document.getElementById('debugCanvas');
-//   const context = canvas.getContext('2d');
-//   const mult = 200;
-//   context.strokeStyle = '#dddddd';
-//   context.beginPath();
-//   context.moveTo(0, 300 + buffer[0] * mult);
-
-//   for (let i = 1; i < buffer.length; i += 2) {
-//     context.lineTo(i, 300 + buffer[i] * mult);
-//   }
-
-//   context.stroke();
-// }
