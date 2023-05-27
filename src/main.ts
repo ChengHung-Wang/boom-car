04ed3469603ed82d2bcb52bd05de7e61e5526131
王正宏 <wangzhenghong@ip-12.wt1.ntust.edu.tw>
2023-05-28 00:28:01 +0800

    安裝 套件

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,21 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// @ts-ignore
 import App from '@/App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'element-plus/dist/index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 
 app.use(createPinia())
+app.use(ElementPlus)
 app.use(router)
 
 app.mount('#app')
