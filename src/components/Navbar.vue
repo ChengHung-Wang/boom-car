f77095b469ba8a9857d304e617b79cda6fce9693
zhian66 <anne920606@gmail.com>
2023-05-29 00:31:03 +0800

    接續前一個 push，新增 Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  const props: any = defineProps(['title'])
+</script>
+
+<template>
+  <nav>
+    <p>icon1</p>
+    <p>{{ props.title }}</p>
+    <p>icon2</p>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
