70ccef087e40f887f5a17e6fe3b2c91518ffa223
zhian66 <anne920606@gmail.com>
2023-05-29 00:28:48 +0800

    建立遊戲開始前主要畫面基本元件， 程式與 figma 頁面對應如下：  - GameMenu.vue：模式選擇  - SinglePlayerMode.vue：單機模式/遊戲設定  - BuildConnections.vue：多人競賽/伺服器位址 & 房號  - MatchMaking.vue：多人競賽/配對畫面
     新增內容：
     - 主要畫面基本元件（不包含css樣式）
     - 按鈕功能（畫面跳躍、文字改變）
     - 後端串接 TODO 標註

---
+++ src/views/GameMode/Multiplayer/MatchMaking.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+  import matchDisplay from "@/components/MatchDisplay.vue";
+</script>
+
+<template>
+  <main>
+    <p>正在為您配對適合的對手中...</p>
+    <p>點擊右上角更換車子樣式讓別人更好的辨識你！</p>
+    <matchDisplay />
+
+    <div>
+      車子/名字畫面顯示
+    </div>
+
+    <!-- TODO:抓取玩家名單動態更新列表   -->
+    <ul>
+      <li>[玩家名稱1]</li>
+      <li>[玩家名稱2]</li>
+      <li>[玩家名稱3]</li>
+      <li>[玩家名稱4]</li>
+      <li>[玩家名稱5]</li>
+    </ul>
+
+
+  </main>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/GameMode/SinglePlayer/SinglePlayerMode.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+import {ref} from "vue";
+
+type Track = {
+  trackName : string,
+  level : string
+}
+
+let trackNumber = ref(0)
+let trackName : string[] = ['山下風景', '白晝廢墟', '星夜城市']
+
+let computerAmount : number = 0
+</script>
+
+<template>
+    <p>單機模式</p>
+    <div>
+        <p>場景選擇</p>
+        <!--   TODO:接收場景選擇，傳送選項到 "../GameView.vue"  startGame([代號])       -->
+        <button @click="trackNumber = 0"> {{ trackName[0] }} </button>
+        <button @click="trackNumber = 1"> {{ trackName[1] }} </button>
+        <button @click="trackNumber = 2"> {{ trackName[2] }} </button>
+        <p>現在選擇的是：{{ trackName[trackNumber] }}</p>
+        <p>難度：簡單</p>
+
+        <!--   TODO:接收電腦數量，傳送    -->
+        <p>電腦數量</p>
+        <input v-model.lazy="computerAmount" type="range" min="0" max="49">
+        <p>現在選擇的電腦數量：{{ computerAmount }}</p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
