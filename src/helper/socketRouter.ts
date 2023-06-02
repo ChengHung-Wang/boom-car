ed5cdbe7b768b3cad501d1afec1a443ae905f7f0
Nolan Liao <nalivvo@gmail.com>
2023-06-02 14:42:36 +0800

    convert methods in socketSync to static

--- src/helper/socketRouter.ts
+++ src/helper/socketRouter.ts
@@ -4,16 +4,16 @@ import socketSync from "./socketSync";
 export function sync(data: DataStruct): void {
     const list: Map<string,(data: DataStruct) => void> = new Map();
 
-    list.set("car-straight", (new socketSync()).carStraight);
-    list.set("car-straight-cancel", (new socketSync()).carStraightCancel);
-    list.set("car-left", (new socketSync()).carLeft);
-    list.set("car-left-cancel", (new socketSync()).carLeftCancel);
-    list.set("car-right", (new socketSync()).carRight);
-    list.set("car-right-cancel", (new socketSync()).carRightCancel);
-    list.set("car-turbo", (new socketSync()).carTurbo);
-    list.set("car-turbo-cancel", (new socketSync()).carTurboCancel);
-    list.set("car-underspeed", (new socketSync()).carUnderSpeed);
-    list.set("car-underspeed-cancel", (new socketSync()).carUnderSpeedCancel);
+    list.set("car-straight", socketSync.carStraight);
+    list.set("car-straight-cancel", socketSync.carStraightCancel);
+    list.set("car-left", socketSync.carLeft);
+    list.set("car-left-cancel", socketSync.carLeftCancel);
+    list.set("car-right", socketSync.carRight);
+    list.set("car-right-cancel", socketSync.carRightCancel);
+    list.set("car-turbo", socketSync.carTurbo);
+    list.set("car-turbo-cancel", socketSync.carTurboCancel);
+    list.set("car-underspeed", socketSync.carUnderSpeed);
+    list.set("car-underspeed-cancel", socketSync.carUnderSpeedCancel);
 
     const target = list.get(<string>data.data?.command);
     if (target) {
--- src/helper/socketSync.ts
+++ src/helper/socketSync.ts
@@ -2,51 +2,51 @@ import type { DataStruct } from "@/services/socket-server/struct";
 import { racer } from "@/services/racer";
 
 export default class socketSync {
-    carStraight(data: DataStruct): void {
+    static carStraight(data: DataStruct): void {
         let playerIndex: number = 1;
         racer.cars[playerIndex].setAccelerate(true);
     }
 
-    carStraightCancel(data: DataStruct): void {
+    static carStraightCancel(data: DataStruct): void {
         let playerIndex: number = 1;
         racer.cars[playerIndex].setAccelerate(false);
     }
 
-    carLeft(data: DataStruct): void {
+    static carLeft(data: DataStruct): void {
         let playerIndex: number = 1;
         racer.cars[playerIndex].setTurnLeft(true);
     }
 
-    carLeftCancel(data: DataStruct): void {
+    static carLeftCancel(data: DataStruct): void {
         let playerIndex: number = 1;
         racer.cars[playerIndex].setTurnLeft(false);
     }
 
-    carRight(data: DataStruct): void {
+    static carRight(data: DataStruct): void {
         let playerIndex: number = 1;
         racer.cars[playerIndex].setTurnRight(true);
     }
 
-    carRightCancel(data: DataStruct): void {
+    static carRightCancel(data: DataStruct): void {
         let playerIndex: number = 1;
         racer.cars[playerIndex].setTurnRight(false);
     }
 
-    carTurbo(data: DataStruct): void {
+    static carTurbo(data: DataStruct): void {
         let playerIndex: number = 1;
         racer.cars[playerIndex].setTurbo(true);
     }
 
-    carTurboCancel(data: DataStruct): void {
+    static carTurboCancel(data: DataStruct): void {
         let playerIndex: number = 1;
         racer.cars[playerIndex].setTurbo(false);
     }
 
-    carUnderSpeed(data: DataStruct): void {
+    static carUnderSpeed(data: DataStruct): void {
 
     }
 
-    carUnderSpeedCancel(data: DataStruct): void {
+    static carUnderSpeedCancel(data: DataStruct): void {
 
     }
 }
