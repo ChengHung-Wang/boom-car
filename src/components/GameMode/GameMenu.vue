46f37c5a8f7a39756d947ff0a74da1c62be46acb
王正宏 <wangzhenghong@wangzhenghongdeiMac.local>
2023-06-23 19:07:04 +0800

    solve path problem when run vite build

--- src/components/GameMode/GameMenu.vue
+++ src/components/GameMode/GameMenu.vue
@@ -2,6 +2,11 @@
 import Card from "@/components/Card.vue";
 import i18n from '@/services/i18n'
 
+const image = [
+    new URL("@/assets/picture/single-card-image.png", import.meta.url),
+    new URL("@/assets/picture/multiplayer-card-image.png", import.meta.url)
+]
+
 const { t } = i18n.global
 </script>
 
@@ -9,12 +14,12 @@ const { t } = i18n.global
     <div class="row" style="top: 100px;">
       <div class="col-sm-12">
         <router-link to="/single" tag="button">
-          <Card class="click" :title="t('Desktop.SelectMode.single_player')" :description="t('Desktop.SelectMode.single_player_annotation')" img="../src/assets/picture/single-card-image.png"></Card>
+          <Card class="click" :title="t('Desktop.SelectMode.single_player')" :description="t('Desktop.SelectMode.single_player_annotation')" :img="image[0]"></Card>
         </router-link>
       </div>
       <div class="col-sm-12">
         <router-link to="/multiplayer/conn" tag="button">
-          <Card class="click" :title="t('Desktop.SelectMode.multi_player')" :description="t('Desktop.SelectMode.multi_player_annotation')" img="../src/assets/picture/multiplayer-card-image.png"></Card>
+          <Card class="click" :title="t('Desktop.SelectMode.multi_player')" :description="t('Desktop.SelectMode.multi_player_annotation')" :img="image[1]"></Card>
         </router-link>
       </div>
     </div>
