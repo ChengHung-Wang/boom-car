31df82e35031e0d572015343a68d3dce24f7227f
王正宏 <wangzhenghong@wangzhenghongdeiMac.local>
2023-06-23 18:56:47 +0800

    release

--- src/helper/helper.ts
+++
@@ -1,4 +0,0 @@
-import DirectionControl from './directionControl'
-import SocketService from "@/helper/socketService";
-export const directionControl: DirectionControl = new DirectionControl();
-export const socket: SocketService = new SocketService();
--- src/services/constants.js
+++
@@ -1,8 +0,0 @@
-export const constants = {};
-constants.KEYUP = 38;
-constants.KEYDOWN = 40;
-constants.KEYLEFT = 37;
-constants.KEYRIGHT = 39;
-constants.helvetica = ' "Helvetica Neue", Helvetica, Arial, sans-serif';
-
-constants.COLORS_LANEMARKER = 0;
